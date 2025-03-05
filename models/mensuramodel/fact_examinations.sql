e4e13b02407c19d416beaa4111fa2128b467d6b5
BarryBeer <156895631+BarryBeer@users.noreply.github.com>
2025-03-05 12:49:07 +0100

    more changes

--- models/mensuramodel/fact_examinations.sql
+++ models/mensuramodel/fact_examinations.sql
@@ -3,6 +3,13 @@ with
 examinations as (
 
     select
-        examination_id
-        
+        planning_id,
+        customer_id,
+        planningentity_id,
+        employee_id,
+        medicalcenter_id,
+        medicalexaminations_Id
+
+
+    from {{ ref('stg_HAW__emloyees_examinations') }}
 )
